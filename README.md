ecc41ea3fcd54b6f97aca352997d7890ac6cdbb4
luckychacha <358140284@qq.com>
2021-09-19 19:20:17 +0800

    erc20

---
+++ README.md
@@ -0,0 +1,32 @@
+1.合约编译截图
+![合约编译截图](./contract_build.png)
+2.canvas-ui 合约内容截图
+![canvas-ui 合约内容截图](./contract_content.png)
+3.dave 授权 alice 可以从 dave 账号中抽取 100。
+```shell
+    allowance
+        |->caller:dave,
+        |->spender:alice,
+        |->value:100
+```
+![canvas-ui 设置授权](./set_allowance.png)
+
+4.由于超出授权金额，所以 transfer_from 失败。
+```shell
+    transfer_from:
+        Call from Account:alice
+        |->dave - 105,
+        |->bob + 105
+        |-->{"err":"InsufficientApproval"}
+```
+![canvas-ui transfer_from 失败](./transfer_from_failed_when_insufficient_approval.png)
+
+5.由于未超出授权金额，所以 transfer_from 成功。
+```shell
+    transfer_from:
+        Call from Account:alice
+        |->dave - 100,
+        |->bob + 100
+        |-->{"ok":[]}
+```
+![canvas-ui transfer_from 成功](./transfer_from_worked.png)
---
+++ tests.rs
@@ -0,0 +1,198 @@
+use super::*;
+use ink_lang as ink;
+
+#[ink::test]
+fn new_works() {
+    let contract = erc20::Erc20::new(100);
+    assert_eq!(contract.total_supply(), 100);
+}
+
+#[ink::test]
+fn total_supply_works() {
+    let contract = erc20::Erc20::new(100);
+    assert_eq!(contract.total_supply(), 100);
+    assert_eq!(contract.balance_of(ink_env::AccountId::from([0x01; 32])), 100);
+    assert_eq!(contract.balance_of(ink_env::AccountId::from([0; 32])), 0);
+}
+
+#[ink::test]
+fn balance_works() {
+    let contract = erc20::Erc20::new(100);
+    assert_eq!(contract.balance_of(ink_env::AccountId::from([0x01; 32])), 100);
+}
+
+#[ink::test]
+fn allowance_works() {
+    let mut contract = erc20::Erc20::new(100);
+    assert_eq!(
+        contract.approve(
+            ink_env::AccountId::from([0x01; 32]),
+            5
+        ),
+        Ok(())
+    );
+    assert_eq!(
+        contract.allowance(
+            ink_env::AccountId::from([0x01; 32]),
+            ink_env::AccountId::from([0x01; 32]),
+        ),
+        5
+    );
+    assert_eq!(
+        contract.allowance(
+            ink_env::AccountId::from([0x01; 32]),
+            ink_env::AccountId::from([0x02; 32]),
+        ),
+        0
+    );
+}
+
+#[ink::test]
+fn transfer_works() {
+    let mut contract = erc20::Erc20::new(100);
+    assert_eq!(
+        contract.transfer(ink_env::AccountId::from([0x02; 32]), 100),
+        Ok(())
+    );
+    assert_eq!(contract.balance_of(ink_env::AccountId::from([0x01; 32])), 0);
+    assert_eq!(contract.balance_of(ink_env::AccountId::from([0x02; 32])), 100);
+}
+
+
+#[ink::test]
+fn transfer_failed_when_insufficient_balance() {
+    let mut contract = erc20::Erc20::new(100);
+    assert_eq!(
+        contract.transfer(ink_env::AccountId::from([0x02; 32]), 101),
+        Err(erc20::Error::InsufficientBalance)
+    );
+    assert_eq!(contract.balance_of(ink_env::AccountId::from([0x01; 32])), 100);
+    assert_eq!(contract.balance_of(ink_env::AccountId::from([0x02; 32])), 0);
+}
+
+#[ink::test]
+fn approve_works() {
+    let mut contract = erc20::Erc20::new(100);
+    assert_eq!(contract.balance_of(ink_env::AccountId::from([0x01; 32])), 100);
+    assert_eq!(
+        contract.approve(ink_env::AccountId::from([0x02; 32]), 100),
+        Ok(())
+    );
+    assert_eq!(
+        contract.allowance(
+            ink_env::AccountId::from([0x01; 32]),
+            ink_env::AccountId::from([0x02; 32])
+        ),
+        100
+    );
+}
+
+#[ink::test]
+fn inner_tranfer_works() {
+    let mut contract = erc20::Erc20::new(100);
+    assert_eq!(
+        contract.inner_tranfer(
+            ink_env::AccountId::from([0x01; 32]),
+            ink_env::AccountId::from([0x02; 32]),
+            50
+        ),
+        Ok(())
+    );
+    assert_eq!(
+        contract.balance_of(ink_env::AccountId::from([0x01; 32])),
+        50
+    );
+    assert_eq!(
+        contract.balance_of(ink_env::AccountId::from([0x02; 32])),
+        50
+    );
+}
+
+
+#[ink::test]
+fn inner_tranfer_failed_when_insufficient_balance() {
+    let mut contract = erc20::Erc20::new(100);
+    assert_eq!(
+        contract.inner_tranfer(
+            ink_env::AccountId::from([0x01; 32]),
+            ink_env::AccountId::from([0x02; 32]),
+            101
+        ),
+        Err(erc20::Error::InsufficientBalance)
+    );
+    assert_eq!(
+        contract.balance_of(ink_env::AccountId::from([0x01; 32])),
+        100
+    );
+    assert_eq!(
+        contract.balance_of(ink_env::AccountId::from([0x02; 32])),
+        0
+    );
+}
+
+#[ink::test]
+fn transfer_from_works() {
+    let mut contract = erc20::Erc20::new(100);
+    let _ = contract.approve(ink_env::AccountId::from([0x01;32]), 50);
+    assert_eq!(
+        contract.transfer_from(
+            ink_env::AccountId::from([0x01; 32]),
+            ink_env::AccountId::from([0x02; 32]),
+            50
+        ),
+        Ok(())
+    );
+    assert_eq!(
+        contract.balance_of(ink_env::AccountId::from([0x01; 32])),
+        50
+    );
+    assert_eq!(
+        contract.balance_of(ink_env::AccountId::from([0x02; 32])),
+        50
+    );
+}
+
+#[ink::test]
+fn transfer_from_failed_when_insufficient_approval() {
+    let mut contract = erc20::Erc20::new(100);
+    let _ = contract.approve(ink_env::AccountId::from([0x01;32]), 50);
+    assert_eq!(
+        contract.transfer_from(
+            ink_env::AccountId::from([0x01; 32]),
+            ink_env::AccountId::from([0x02; 32]),
+            51
+        ),
+        Err(erc20::Error::InsufficientApproval)
+    );
+    assert_eq!(
+        contract.balance_of(ink_env::AccountId::from([0x01; 32])),
+        100
+    );
+    assert_eq!(
+        contract.balance_of(ink_env::AccountId::from([0x02; 32])),
+        0
+    );
+}
+
+
+#[ink::test]
+fn transfer_from_failed_when_insufficient_balance() {
+    let mut contract = erc20::Erc20::new(100);
+    let _ = contract.approve(ink_env::AccountId::from([0x01;32]), 200);
+    assert_eq!(
+        contract.transfer_from(
+            ink_env::AccountId::from([0x01; 32]),
+            ink_env::AccountId::from([0x02; 32]),
+            101
+        ),
+        Err(erc20::Error::InsufficientBalance)
+    );
+    assert_eq!(
+        contract.balance_of(ink_env::AccountId::from([0x01; 32])),
+        100
+    );
+    assert_eq!(
+        contract.balance_of(ink_env::AccountId::from([0x02; 32])),
+        0
+    );
+}
